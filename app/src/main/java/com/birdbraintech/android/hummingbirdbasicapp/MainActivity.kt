c51602b91bc0ded3503d6309d80e6ba00dd4147d
bambi <bambi@birdbraintechnologies.com>
2020-07-24 16:15:03 -0400

    Implemented main activity screen to control lights and motors while viewing the values of the Hummingbird sensors

--- app/src/main/java/com/birdbraintech/android/hummingbirdbasicapp/MainActivity.kt
+++ app/src/main/java/com/birdbraintech/android/hummingbirdbasicapp/MainActivity.kt
@@ -2,10 +2,12 @@ package com.birdbraintech.android.hummingbirdbasicapp
 
 import android.os.Bundle
 import android.view.View
+import android.widget.SeekBar
 import androidx.appcompat.app.AppCompatActivity
 import com.birdbraintech.android.hummingbirdbasicapp.Hummingbird.Hummingbird
 import com.birdbraintech.android.hummingbirdbasicapp.Hummingbird.HummingbirdApplication
 import kotlinx.android.synthetic.main.activity_main.*
+import kotlin.math.roundToInt
 
 /* This controls the main screen of the app that allows you to control the lights and motors of
 the Hummingbird. It also displays the Hummingbird sensor values and the connection status. */
@@ -16,12 +18,167 @@ class MainActivity : AppCompatActivity(), Hummingbird.HummingbirdListener {
     val hummingbird: Hummingbird?
         get() = (application as HummingbirdApplication).hummingbird
 
+    var lightsOn = false    // whether or not single color LEDs 1-3 are on
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         /* Make this the activity that is receiving Hummingbird data */
         hummingbird?.setHummingbirdListener(this)
+
+        /* Set up the seek bars. */
+        val seekBarColor: SeekBar = findViewById(R.id.seekBarColor)
+        seekBarColor?.setOnSeekBarChangeListener(object :
+            SeekBar.OnSeekBarChangeListener {
+            override fun onProgressChanged(seek: SeekBar, progress: Int, fromUser: Boolean) {
+                // write custom code for progress is changed on seekbar
+
+            }
+
+            override fun onStartTrackingTouch(seek: SeekBar) {
+                // write custom code for progress is started on seekbar
+            }
+
+            override fun onStopTrackingTouch(seek: SeekBar) {
+                // When progress is stopped on the seekbar, we want to set the lights of the
+                // Finch beak and tail based on its value
+                when (seek.progress) {
+                    in 0..12 -> {
+                        hummingbird?.setTriLED(1, 100,100,100)
+                        hummingbird?.setTriLED(2, 100,100,100)
+                    }
+                    in 13..24 -> {
+                        hummingbird?.setTriLED(1, 100,100,0)
+                        hummingbird?.setTriLED(2, 100,100,0)
+                    }
+                    in 25..36 -> {
+                        hummingbird?.setTriLED(1, 0,100,100)
+                        hummingbird?.setTriLED(2,0,100,100)
+                    }
+                    in 37..48 -> {
+                        hummingbird?.setTriLED(1, 0,100,0)
+                        hummingbird?.setTriLED(2,0,100,0)
+                    }
+                    in 49..60 -> {
+                        hummingbird?.setTriLED(1, 100,0,100)
+                        hummingbird?.setTriLED(2, 100,0,100)
+                    }
+                    in 61..72 -> {
+                        hummingbird?.setTriLED(1, 100,0,0)
+                        hummingbird?.setTriLED(2, 100,0,0)
+                    }
+                    in 73..84 -> {
+                        hummingbird?.setTriLED(1, 0,0,100)
+                        hummingbird?.setTriLED(2, 0,0,100)
+                    }
+                    else -> {
+                        hummingbird?.setTriLED(1, 0,0,0)
+                        hummingbird?.setTriLED(2, 0,0,0)
+                    }
+                }
+            }
+        })
+        seekBarColor.progress = 100
+
+        val seekBarServo1: SeekBar = findViewById(R.id.seekBarServo1)
+        seekBarServo1?.setOnSeekBarChangeListener(object :
+            SeekBar.OnSeekBarChangeListener {
+            override fun onProgressChanged(seek: SeekBar, progress: Int, fromUser: Boolean) {
+                // write custom code for progress is changed on seekbar
+
+            }
+
+            override fun onStartTrackingTouch(seek: SeekBar) {
+                // write custom code for progress is started on seekbar
+            }
+
+            override fun onStopTrackingTouch(seek: SeekBar) {
+                // When progress is stopped on the seekbar, we want to set the lights of the
+                // Finch beak and tail based on its value
+                hummingbird?.setPositionServo(1,(1.8*seek.progress).roundToInt())
+
+            }
+        })
+
+        val seekBarServo2: SeekBar = findViewById(R.id.seekBarServo2)
+        seekBarServo2?.setOnSeekBarChangeListener(object :
+            SeekBar.OnSeekBarChangeListener {
+            override fun onProgressChanged(seek: SeekBar, progress: Int, fromUser: Boolean) {
+                // write custom code for progress is changed on seekbar
+
+            }
+
+            override fun onStartTrackingTouch(seek: SeekBar) {
+                // write custom code for progress is started on seekbar
+            }
+
+            override fun onStopTrackingTouch(seek: SeekBar) {
+                // When progress is stopped on the seekbar, we want to set the lights of the
+                // Finch beak and tail based on its value
+                hummingbird?.setPositionServo(2,(1.8*seek.progress).roundToInt())
+
+            }
+        })
+
+        val seekBarServo3: SeekBar = findViewById(R.id.seekBarServo3)
+        seekBarServo3?.setOnSeekBarChangeListener(object :
+            SeekBar.OnSeekBarChangeListener {
+            override fun onProgressChanged(seek: SeekBar, progress: Int, fromUser: Boolean) {
+                // write custom code for progress is changed on seekbar
+
+            }
+
+            override fun onStartTrackingTouch(seek: SeekBar) {
+                // write custom code for progress is started on seekbar
+            }
+
+            override fun onStopTrackingTouch(seek: SeekBar) {
+                // When progress is stopped on the seekbar, we want to set the lights of the
+                // Finch beak and tail based on its value
+                hummingbird?.setPositionServo(3,(1.8*seek.progress).roundToInt())
+
+            }
+        })
+
+        val seekBarServo4: SeekBar = findViewById(R.id.seekBarServo4)
+        seekBarServo4?.setOnSeekBarChangeListener(object :
+            SeekBar.OnSeekBarChangeListener {
+            override fun onProgressChanged(seek: SeekBar, progress: Int, fromUser: Boolean) {
+                // write custom code for progress is changed on seekbar
+
+            }
+
+            override fun onStartTrackingTouch(seek: SeekBar) {
+                // write custom code for progress is started on seekbar
+            }
+
+            override fun onStopTrackingTouch(seek: SeekBar) {
+                // When progress is stopped on the seekbar, we want to set the lights of the
+                // Finch beak and tail based on its value
+                hummingbird?.setPositionServo(4,(1.8*seek.progress).roundToInt())
+
+            }
+        })
+    }
+
+    /* This function toggles the single color LEDs when the light button is pressed. */
+    fun lightButtonClicked(view: View) {
+        if (lightsOn) {
+            hummingbird?.setLED(1,0)
+            hummingbird?.setLED(2,0)
+            hummingbird?.setLED(3,0)
+        } else {
+            hummingbird?.setLED(1,100)
+            hummingbird?.setLED(2,100)
+            hummingbird?.setLED(3,100)
+        }
+        lightsOn = !lightsOn
+    }
+
+    /* This function plays a random music note when the music button is pressed. */
+    fun musicButtonClicked(view: View) {
+        hummingbird?.playNote((32..135).random(),0.5)
     }
 
     /* This Bluetooth does not disconnect automatically, so I want to disconnect it if this activity stops (since there are no
@@ -54,19 +211,6 @@ class MainActivity : AppCompatActivity(), Hummingbird.HummingbirdListener {
             this.distanceNumber.text = hummingbird?.sensorState?.sensor1?.distance.toString() + " cm"
             this.lightNumbers.text = hummingbird?.sensorState?.sensor2?.light.toString()
             this.lineNumbers.text = hummingbird?.sensorState?.sensor3?.dial.toString()
-            this.accelerationNumbers.text =
-                "(" + hummingbird?.sensorState?.acceleration?.get(0).toString() + ", " +
-                        hummingbird?.sensorState?.acceleration?.get(1).toString() + ", " +
-                        hummingbird?.sensorState?.acceleration?.get(2).toString() + ")"
-            this.magnetometerNumbers.text =
-                "(" + hummingbird?.sensorState?.magnetometer?.get(0).toString() + ", " +
-                        hummingbird?.sensorState?.magnetometer?.get(1).toString() + ", " +
-                        hummingbird?.sensorState?.magnetometer?.get(2).toString() + ")"
-            this.compassNumber.text = hummingbird?.sensorState?.compass?.toString() + "°"
-            this.buttonValues.text = "(" + hummingbird?.sensorState?.buttonA.toString() + ", " +
-                    hummingbird?.sensorState?.buttonB.toString() + ", " +
-                    hummingbird?.sensorState?.shake.toString() + ")"
-            this.batteryNumber.text = hummingbird?.sensorState?.batteryVoltage.toString() + " V"
         }))
 
     }
